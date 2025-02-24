2983a1fb93be836c6e478c71bbb0cdf76c8d2067
theiann <2015iann@gmail.com>
2025-02-24 13:43:45 -0600

    Added constructor and methods for MiddleEarthCharacter class

--- src/characters/MiddleEarthCharacter.java
+++ src/characters/MiddleEarthCharacter.java
@@ -1,5 +1,22 @@
 package characters;
 
 public abstract class MiddleEarthCharacter {
-
+	private String name;
+	private double health;
+	private double power;
+	
+	
+	public MiddleEarthCharacter(String n, double h, double p) {
+		this.name = n;
+		this.health = h;
+		this.power = p;
+	}
+	
+	public abstract boolean attack(MiddleEarthCharacter target);
+	
+	public abstract String getRace();
+	
+	public void displayInfo() {
+		System.out.println("Name: " + this.name + " Health: " + this.health + " Power: " + this.power);
+	}
 }
