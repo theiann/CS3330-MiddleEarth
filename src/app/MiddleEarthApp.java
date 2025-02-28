2a3f9707bc43614ef760f877c493984cf6cd4bc5
theiann <2015iann@gmail.com>
2025-02-28 12:19:31 -0600

    Added Orc subclass

--- src/app/MiddleEarthApp.java
+++ src/app/MiddleEarthApp.java
@@ -1,16 +1,18 @@
 package app;
 //import manager.MiddleEarthCouncil;
-import characters.Elf;
-import characters.Dwarf;
+import characters.*;
+
 
 public class MiddleEarthApp {
 
 	public static void main(String[] args) {
 		Elf char1 = new Elf("elf", 100, 10);
 		Dwarf char2 = new Dwarf("dwarf", 100, 10);
-		
+		Human char3 = new Human("human", 100, 20);
+		Orc char4 = new Orc("orc", 100, 30);
 		System.out.println(char2.attack(char1));
 		System.out.println(char1.getHealth());
+		System.out.println(char4.attack(char3));
 	}
 
 }
--- src/characters/Orc.java
+++ src/characters/Orc.java
@@ -1,5 +1,73 @@
 package characters;
 
 public class Orc extends MiddleEarthCharacter {
-
+	/**
+	 * 
+	 * This method will construct a new Orc subclass with default values.
+	 * 
+	 */
+	public Orc() {
+		this.name = "Unknown";
+		this.health = 100;
+		this.power = 10;
+	}
+	
+	/**
+	 * This method will construct a new Orc subclass with provided values.
+	 * @param n 
+	 * 			Name for new character
+	 * @param h
+	 * 			Health for new character
+	 * @param p
+	 * 			Power for new character
+	 * 
+	 */
+	public Orc(String n, double h, double p) {
+		this.name = n;
+		this.health = h;
+		this.power = p;
+	}
+	
+	
+	/**
+	 * 
+	 * This method will attack a targeted character.
+	 * @param target
+	 * 			Targeted character
+	 * @return
+	 * 			Returns false if the attack failed
+	 * 			Returns true if the attack went through successfully
+	 */
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		double oldHealth = target.getHealth();
+		double health = target.getHealth();
+		double scalar = 1.0;
+		
+		
+		if((target.getRace() != this.getRace()) && (target.getRace() != "Elf")){
+			if(target.getRace() == "Human") {
+				scalar += 0.5;
+			}
+			health -= this.power * scalar;
+			target.setHealth(health);
+			System.out.println(this.getName() + " (" + this.getHealth() + ")" + " attacked " + target.getName() 
+			+ " (" + target.getHealth() + ") with " + this.power*scalar + " damage! (" + this.power + "x" + scalar + ") (HP: " + oldHealth + " -> " + health + ")"); 
+			return true;
+		}
+		System.out.println(this.getName() + " (" + this.getHealth() + ")" + " tried to attack " + target.getName() + " (" + this.getHealth() + ") but the attack failed!");
+		return false;
+	}
+	
+	
+	/**
+	 * 
+	 * This method will get the Race of the subclass.
+	 * @return
+	 * 			Returns name of the race
+	 */
+	@Override
+	public String getRace() {
+		return "Orc";
+	}
 }
