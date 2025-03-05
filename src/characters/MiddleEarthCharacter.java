b15ccb95731ce536c277336b0639503b8890e813
theiann <2015iann@gmail.com>
2025-03-05 17:29:54 -0600

    Added javadoc to MiddleEarthCouncil and MiddleEarthCharacter

--- src/characters/MiddleEarthCharacter.java
+++ src/characters/MiddleEarthCharacter.java
@@ -20,24 +20,78 @@ public abstract class MiddleEarthCharacter {
 	
 	public abstract String getRace();
 	
+	
+	/**
+	 * This method will add display the info for a MiddleEarthCharacter
+	 */
 	public void displayInfo() {
 		System.out.println("Name: " + this.name + " Health: " + this.health + " Power: " + this.power);
 	}
+	
+	
+	/**
+	 * 
+	 * This method will get the name of the MiddleEarthCharacter 
+	 * 
+	 * @return
+	 * 				Returns name of the MiddleEarthCharacter
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * 
+	 * This method will set the name of the MiddleEarthCharacter 
+	 * 
+	 * @param
+	 * 				Name of the MiddleEarthCharacter
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * 
+	 * This method will get the health of the MiddleEarthCharacter 
+	 * 
+	 * @return
+	 * 				Returns health of the MiddleEarthCharacter
+	 */
 	public double getHealth() {
 		return health;
 	}
+	
+	/**
+	 * 
+	 * This method will set the health of the MiddleEarthCharacter 
+	 * 
+	 * @param
+	 * 				Health of the MiddleEarthCharacter
+	 */
 	public void setHealth(double health) {
 		this.health = health;
 	}
+	
+	
+	/**
+	 * 
+	 * This method will get the power of the MiddleEarthCharacter 
+	 * 
+	 * @return
+	 * 				Returns power of the MiddleEarthCharacter
+	 */
 	public double getPower() {
 		return power;
 	}
+	
+	/**
+	 * 
+	 * This method will set the power of the MiddleEarthCharacter 
+	 * 
+	 * @param
+	 * 				Power of the MiddleEarthCharacter
+	 */
 	public void setPower(double power) {
 		this.power = power;
 	}
--- src/manager/MiddleEarthCouncil.java
+++ src/manager/MiddleEarthCouncil.java
@@ -5,11 +5,16 @@ public class MiddleEarthCouncil {
 	private static MiddleEarthCouncil instance;
 	private CharacterManager manager;
 	
-	
+	/**
+	 * This method will make the first instance of MiddleEarthCouncil
+	 */
 	private MiddleEarthCouncil() {
 		this.manager = new CharacterManager();
 	}
 	
+	/**
+	 * This method gets the instance of MiddleEarthCouncil and if one has not been made, it will be.
+	 */
 	public static MiddleEarthCouncil getInstance() {
 		if (instance == null) {
 			instance = new MiddleEarthCouncil();
@@ -17,7 +22,15 @@ public class MiddleEarthCouncil {
 		return instance;
 	}
 	
-	CharacterManager getCharacterManager() {
+	
+	/**
+	 * 
+	 * This method will get the character manager.
+	 * 
+	 * @return
+	 * 			returns the instance of the character manager
+	 */
+	public CharacterManager getCharacterManager() {
 		return this.manager;
 	}
 	
